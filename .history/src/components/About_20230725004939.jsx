da2e308fc369505cb9eae562c07b401ca724abe4
Dragon <dragonKnight0522@gmail.com>
2023-08-10 01:32:22 +0900

    asdasd

---
+++ .history/src/components/About_20230725004939.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{ max: 45, scale: 1, speed: 450, }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Highly skilled and dedicated <span className='text-yellow-600'>blockchain developer</span> with <span className='text-red-700'>+ 4</span> years of experiences in developing decentralized applications (DApps) and smart contracts. Proficient in blockchain technologies, including Ethereum, with a deep understanding of cryptography and consensus algorithms.
+        <br />
+        I am also a <span className='text-black'>web developer</span> with a passion for creating innovative and user-friendly websites. Proficient in front-end, <span className='text-red-700'>+ 8</span> years of web developing experiences and programming languages. Experienced in collaborating with cross-functional teams to deliver high-quality projects within deadlines. Committed to staying updated with the latest industry trends and continuously improving skills to provide exceptional web solutions.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ .history/src/constants/index_20230810010150.js
@@ -0,0 +1,286 @@
+import {
+  blockchain,
+  frontend,
+  backend,
+  ethereum,
+  ethers,
+  solidity,
+  truffle,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'skills',
+    title: 'Skills',
+  },
+  {
+    id: 'experience',
+    title: 'Experiences',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'GraphQL',
+    icon: graphql,
+  },
+  {
+    name: 'Node.js',
+    icon: nodejs,
+  },
+  {
+    name: 'MongoDB',
+    icon: mongoDB,
+  },
+  {
+    name: 'MySQL',
+    icon: mySQL,
+  },
+  {
+    name: 'PostgreSQL',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'HTML',
+    icon: html,
+  },
+  {
+    name: 'CSS',
+    icon: css,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'Blockchain',
+    icon: blockchain,
+  },
+  {
+    name: 'Ethereum',
+    icon: ethereum,
+  },
+  {
+    name: 'Solidity',
+    icon: solidity,
+  },
+  {
+    name: 'Truffle',
+    icon: truffle,
+  },
+  {
+    name: 'Ethers JS',
+    icon: ethers,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Blockchain Developer',
+    company_name: 'Collabera',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: '09 / 21 - 07 / 2023',
+  },
+  {
+    title: 'Ethereum Developer',
+    company_name: 'Centrys, Inc',
+    icon: microverse,
+    iconBg: '#333333',
+    date: '08 / 2019 - 08 / 2021',
+  },
+  {
+    title: 'Front End Developer',
+    company_name: 'KickFin',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: '07 / 2017 - 09 / 2018',
+  },
+  {
+    title: 'Java Developer',
+    company_name: 'Germany Trade & Invest',
+    icon: dcc,
+    iconBg: '#333333',
+    date: '07 / 2015 - 06 / 2017',
+  },
+  {
+    title: 'Software Test Engineer (Internship)',
+    company_name: 'Germany Trade & Invest',
+    icon: dcc,
+    iconBg: '#333333',
+    date: '07 / 2014 - 06 / 2015',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'DefiPrime',
+    description: 'DeFiprime.com is the digital media and analytical services provider for the DeFi community. Its mandate is to inform, educate, and connect the community as the definitive source of news dedicated to the decentralized finance space.',
+    tags: [
+      {
+        name: 'DeFi',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Ethereum',
+        color: 'green-text-gradient',
+      }
+    ],
+    image: komikult,
+    repo: 'https://github.com/defiprime/defiprime',
+    demo: 'https://defiprime.com/',
+  },
+  {
+    id: 'project-2',
+    name: 'Stereum 2.0',
+    description:
+      'Stereum manages the process of setting up & maintaining an Ethereum node for you with a focus on self sovereignty, privacy and flexibility.',
+    tags: [
+      {
+        name: 'Ethereum',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Stacking',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/stereum-dev/ethereum-node',
+    demo: 'https://stereum.net/',
+  },
+  {
+    id: 'project-3',
+    name: 'NFT Suits',
+    description: 'This is a Nft game',
+    tags: [
+      {
+        name: 'Ethereum',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'NFT',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/NFTSuits/NFT-Marketplace-DApp',
+    demo: 'http://ww25.nftsuits.com/',
+  },
+  {
+    id: 'project-4',
+    name: 'BitFact',
+    description: `Prove data with this complete toolkit. BitFact fingerprints information and then etches it on the Ethereum Blockchain.`,
+    tags: [
+      {
+        name: 'ethereum',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'blockchain',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/zachalam/BitFact',
+    demo: 'https://docs.bitfact.io/#/',
+  },
+  {
+    id: 'project-5',
+    name: 'Recurring Payments',
+    description:
+      'Recurring payments on the blockchain have been a topic of discussion for some time. First introduced in EIP-1337 in 2018, the proposal never really caught on. My approach to recurring payments on Ethereum takes a simpler approach than EIP-1337 did, which may help it have a larger impact on the community.',
+    tags: [
+      {
+        name: 'ethereum',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'Blockchain',
+        color: 'green-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/Jon-Becker/ethereum-recurring-payments',
+    demo: 'https://jbecker.dev/research/ethereum-recurring-payments/',
+  },
+];
+
+export { services, technologies, experiences, projects };
